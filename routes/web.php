16bc1e1169170e65912f77e3425d8d6a899b0201
falcon-ov <vinifon4he@gmail.com>
2025-06-15 12:27:37 +0300

    feat: update structure

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,10 @@
 <?php
-require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../models/Task.php';
-require_once __DIR__ . '/../controllers/TaskController.php';
+require_once __DIR__ . '/../database/database.php';
+require_once __DIR__ . '/../app/models/Task.php';
+require_once __DIR__ . '/../app/controllers/TaskController.php';
 
 $db = new Database();
 $taskModel = new Task($db->getConnection());
 $controller = new TaskController($taskModel);
 
-$action = $_GET['action'] ?? 'index';
-
-switch ($action) {
-    case 'create':
-        //$controller->create();
-        break;
-    case 'store':
-        //$controller->store();
-        break;
-    default:
-       // $controller->index();
-        break;
-}
+echo "<br> ROUTE";
